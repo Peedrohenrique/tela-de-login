d6ae04bb88996200acb310c367fee38a94363c88
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2021-08-09 23:47:56 -0300

    Tela de login
    Colocado um user e Password no JAVASCRIPT
    
    Modificado um Label e add  um id T

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ function logar(){
     const login = document.getElementById('login')
     const senha = Number(document.getElementById('senha'))
     const mudar = document.getElementById('mudar')
-    if(login.value == 'Pedro'||'hendrio' && senha.value == '34970994'){
+    if(login.value == 'Pedro' || 'Hendrio' && senha.value == '34970994' || senha.value == 'pedro'){
         localStorage.setItem("acesso",true);
         alert("usuário autenticado!")
         window.location.href = "https://www.google.com/"
     } else{
-        mudar.innerHTML = 'Usuário ou Senha inválidos! <br>'
-        mudar.innerHTML += 'Tente novamente.'
+        
+        mudar.innerHTML = '<br><center style="font-size:17px"> Usuário ou Senha inválidos!</center>'
+        mudar.innerHTML += '<center style="font-size:17px">Tente novamente.</center>'
     }
 }

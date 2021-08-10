92f334bd01bd81aecb4c8e9d611fa8e3ae820815
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2021-08-10 09:19:34 -0300

    Tela de login
    Adicionado um user

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 function logar(){
     const login = document.getElementById('login')
-    const senha = Number(document.getElementById('senha'))
+    const senha = document.getElementById('senha')
     const mudar = document.getElementById('mudar')
-    if(login.value == 'Pedro' || 'Hendrio' && senha.value == '34970994' || senha.value == 'pedro'){
+    if(login.value == 'Pedro' || login.value == 'Hendrio' && senha.value == '34970994' || senha.value == 'pedro'){
         localStorage.setItem("acesso",true);
         alert("usuário autenticado!")
         window.location.href = "https://www.google.com/"
+    } else if (login.value == 'rodrigo' || senha.value == '784512'){
+        localStorage.setItem("acesso",true);
+        alert("usuário autenticado!")
+        window.location.href = "https://www.google.com/"
+       
     } else{
-        
         mudar.innerHTML = '<br><center style="font-size:17px"> Usuário ou Senha inválidos!</center>'
         mudar.innerHTML += '<center style="font-size:17px">Tente novamente.</center>'
     }

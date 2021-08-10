7dc28d7914274f67ea8117f958c5aea9e0a05eca
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2021-08-10 16:28:49 -0300

    Tela de login
    Arrumado o Bug da autenticação sem senha

--- js/main.js
+++ js/main.js
@@ -2,17 +2,12 @@ function logar(){
     const login = document.getElementById('login')
     const senha = document.getElementById('senha')
     const mudar = document.getElementById('mudar')
-    if(login.value == 'Pedro' || login.value == 'Hendrio' && senha.value == '34970994' || senha.value == 'pedro'){
+    if(login.value == "Pedro" || "Rodrigo" && senha.value == "34970994" || senha.value == "784512"){
         localStorage.setItem("acesso",true);
         alert("usuário autenticado!")
         window.location.href = "https://www.google.com/"
-    } else if (login.value == 'rodrigo' || senha.value == '784512'){
-        localStorage.setItem("acesso",true);
-        alert("usuário autenticado!")
-        window.location.href = "https://www.google.com/"
-       
-    } else{
+    } else {
         mudar.innerHTML = '<br><center style="font-size:17px"> Usuário ou Senha inválidos!</center>'
         mudar.innerHTML += '<center style="font-size:17px">Tente novamente.</center>'
-    }
+    } 
 }

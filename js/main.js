5edc5aa079833bfaecd8a3e04dac41796687f8cc
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2021-08-11 22:43:57 -0300

    Tela de login 1.3
    Colocado uma validação com variável JSON
    
    e mudado alguns estilos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-function logar(){
+/*variável JSON.*/
+var usuarios = [
+    { "login": "pedro", "senha": "34970994" },
+    { "login": "rodrigo", "senha": "rodrigo" },
+    { "login": "henrique", "senha": "henrique" },
+    { "login": "sarah", "senha": "teamopedro" },
+];
+
+function Login() {
+    var usuario = document.getElementsByName('username')[0].value.toLowerCase();
+    var senha = document.getElementsByName('password')[0].value;
+    const mudar = document.getElementById('mudar')
+
+    for (var u in usuarios) {
+        var us = usuarios[u];
+        if (us.login === usuario && us.senha === senha) {
+            localStorage.setItem("acesso", true);
+            alert("usuário autenticado!")
+            window.location.href = "https://www.google.com/"
+            return true;
+        }
+    }
+    mudar.innerHTML = '<br><center style="font-size:17px"> Usuário ou Senha inválidos!</center>'
+    mudar.innerHTML += '<center style="font-size:17px">Tente novamente.</center>'
+    return false;
+}
+
+
+
+/*function logar(){
     const login = document.getElementById('login')
     const senha = Number(document.getElementById('senha'))
     const mudar = document.getElementById('mudar')
@@ -7,8 +36,9 @@ function logar(){
         alert("usuário autenticado!")
         window.location.href = "https://www.google.com/"
     } else{
-        
+
         mudar.innerHTML = '<br><center style="font-size:17px"> Usuário ou Senha inválidos!</center>'
         mudar.innerHTML += '<center style="font-size:17px">Tente novamente.</center>'
     }
 }
+*/
